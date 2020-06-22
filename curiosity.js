ad190835374d687b46f34afc087eb358febc8fbb
MaxBueno <maxhugobueno@gmail.com>
2020-06-22 00:16:32 -0500

    curiosity
    function curiosity

---
+++ curiosity.js
@@ -0,0 +1,10 @@
+function ord(str) { 
+    return str.charCodeAt(0); 
+}
+
+function chr(number){
+    return String.fromCharCode(number); 
+}
+
+//SEE
+//questions/10333098/utf-8-safe-equivalent-of-ord-or-charcodeat-in-php
